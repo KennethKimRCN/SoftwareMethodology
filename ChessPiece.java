75034efa594fa04293a5d5ac93a03acbbd3bb8b3
WHITNEY POH <wp143@scarletmail.rutgers.edu>
2019-10-18 23:05:31 +0000

    ChessPiece.java edited online with Bitbucket

--- ChessPiece.java
+++ ChessPiece.java
@@ -1,8 +1,15 @@
 public class ChessPiece {
+	static ChessSquare[][] chessBoard;
+	
 	int xpos;
 	int ypos;
+	char team;
 	
 	public boolean move() {
 		return false;
+	}
+	
+	public char getTeam() {
+		return team;
 	}
 }
