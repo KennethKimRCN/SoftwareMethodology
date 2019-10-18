9b9f2cfc78e943256fa495dcec7d230517777284
WHITNEY POH <wp143@scarletmail.rutgers.edu>
2019-10-18 23:04:51 +0000

    ChessSquare.java edited online with Bitbucket

--- ChessSquare.java
+++ ChessSquare.java
@@ -4,11 +4,30 @@ public class ChessSquare {
 	
 	public ChessSquare() {
 		isOccupied = false;
+		piece = null;
 	}
 	
 	public ChessSquare(ChessPiece x) {
 		isOccupied = true;
 		piece = x;
+	}
+	
+	public boolean occupied() {
+		return isOccupied;
+	}
+	
+	public void unoccupy() {
+		piece = null;
+		isOccupied = false;
+	}
+	
+	public void occupy(ChessPiece x) {
+		piece = x;
+		isOccupied = true;
+	}
+	
+	public ChessPiece getPiece() {
+		return piece;
 	}
 
 }
