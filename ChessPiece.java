7e884ce77daa8824b8a0281a428011a162bbf9a3
WHITNEY POH <wp143@scarletmail.rutgers.edu>
2019-10-27 18:42:58 +0000

    ChessPiece.java edited online with Bitbucket

--- ChessPiece.java
+++ ChessPiece.java
@@ -1,9 +1,30 @@
 public class ChessPiece {
-	static ChessSquare[][] chessBoard;
 	
 	int xpos;
 	int ypos;
 	char team;
+	
+	public boolean canMove(ChessSquare[][] chessBoard) {
+		for(int i=0;i<8;i++) {
+			for(int j=0;j<8;j++) {
+				if(canMove(i,j,chessBoard)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public boolean canMove(int x, int y, ChessSquare[][] chessBoard) {
+		ChessPiece a = this;
+		
+		if(a.move(x,y,chessBoard)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 	
 	public ChessPiece(int x, int y, char t) {
 		xpos = x;
@@ -11,7 +32,7 @@ public class ChessPiece {
 		team = t;
 	}
 	
-	public boolean move() {
+	public boolean move(int x,int y,ChessSquare[][] chessBoard) {
 		return false;
 	}
 	
