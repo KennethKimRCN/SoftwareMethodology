82bb065e6899447aa75bfc7efd8ef8da01c82ff3
57RCN <rkdsus93@gmail.com>
2019-11-19 19:55:54 -0500

    fixed problem with starting up the app

--- Photos63/src/app/Photos.java
+++ Photos63/src/app/Photos.java
@@ -1,89 +1,50 @@
 package app;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
 /**
  * @author Khangnyon Kim
  * @author Whitney Poh
- * 
- * This is the driver class
  */
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import controller.Login;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-
-import model.Admin;
-import model.User;
-import model.UserList;
-
 public class Photos extends Application {
-	private UserList usersObject;
+	
 	/**
-	 * Launcher
+	 * This is the main driver class for the software
+	 * Troubleshooting: If the software does not start up, you must initialize the admin account first. 
 	 */
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		this.usersObject = this.getUsersContainer();
-
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("/view/Login.fxml"));
-
-		AnchorPane root = (AnchorPane)loader.load();
-
-		Login Controller = loader.getController();
-		Controller.start(primaryStage, this.usersObject);
-
-		Scene scene = new Scene(root, 360, 280);
-		primaryStage.setTitle("Photos63 by Khangnyon Kim and Whitney Poh");
-		primaryStage.setResizable(false);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
-
-	@Override
-	public void stop() {
+	public void start(Stage primaryStage) throws IOException, ClassNotFoundException {
+		//Initialize user file so it won't crash when executed
+		/*
+		UserList ulist = new UserList();
+		ulist.addUserToList(new User("admin"));
+		UserList.write(ulist);
+		System.out.println("Initialization successful!");
+		*/
+		
 		try {
-			UserList.writeUserList(this.usersObject);
-		} catch(IOException e) {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/view/Login.fxml"));
+			BorderPane root = (BorderPane)loader.load();
+			Scene scene = new Scene(root, 360, 640);
+			
+			
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Photo63 by Khangnyon Kim and Whitney Poh");
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	/**
-	 * Main driver
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-	/**
-	 * This function stores data
-	 * 
-	 */
-	private UserList getUsersContainer() {
-		UserList usersContainer = null;
-		try {
-			usersContainer = UserList.readUserList();
-		} catch (FileNotFoundException e) {
-			usersContainer = new UserList();
-			User adminUser = new Admin();
-			usersContainer.addUser(adminUser);
-			try {
-				UserList.writeUserList(usersContainer);
-			} catch (IOException f) {
-				f.printStackTrace();
-			}
-		} catch (ClassNotFoundException | IOException e) {
-			e.printStackTrace();	
-		}
-		return usersContainer;
-	}
-	
-	
-
 }
